b01c366f9a952bc7a5bb964f0cf7c03ca75293de
Dmitry Fedyuk <dfediuk@gmail.com>
2019-03-16 17:41:17 +0600

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,7 +1,7 @@
 // 2019-03-15
 // "Show the «Sorry for the inconvenience» popup
 // if a customer tries to use the PayPal or Amazon Pay payment method
-// for an order with an «autoshipment» product": https://github.com/mineralair/core/issues/
+// for an order with an «autoshipment» product": https://github.com/mineralair/core/issues/2
 define(['jquery', 'Mineralair_Core/modal', 'domReady!'], function($, modal) {return function() {
 	$('a[data-action=paypal-in-context-checkout] > img').click(function(e) {
 		e.preventDefault();
--- view/frontend/web/modal.js
+++ view/frontend/web/modal.js
@@ -1,9 +1,9 @@
 // 2019-03-16
 // "Show the «Sorry for the inconvenience» popup
 // if a customer tries to use the PayPal or Amazon Pay payment method
-// for an order with an «autoshipment» product": https://github.com/mineralair/core/issues/
+// for an order with an «autoshipment» product": https://github.com/mineralair/core/issues/2
 define(['jquery', 'Magento_Ui/js/modal/modal'], function($) {return function() {
-	var $content = $('.mnr-modal');
+	var $content = $('.mnr-modal-content');
 	// 2018-05-21
 	// How is the `mage.modal` jQuery UI widget implemented and used?
 	// https://mage2.pro/t/5518
@@ -20,7 +20,7 @@ define(['jquery', 'Magento_Ui/js/modal/modal'], function($) {return function() {
 				,text: $.mage.__('No, I will choose a different payment method')
 			}
 		]
-		,modalClass: 'albumenvy-popup'
+		,modalClass: 'mnr-modal'
 		,type: 'popup'
 	});
 	m.modal('openModal');
